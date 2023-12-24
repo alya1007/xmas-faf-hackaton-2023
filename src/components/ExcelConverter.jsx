a3f2a70e39b03569aecbd72d304d87e489aa3cbd
micha <michael.echim@gmail.com>
2023-12-24 10:33:27 +0200

    conflict and warning checking

--- src/components/ExcelConverter.jsx
+++ src/components/ExcelConverter.jsx
@@ -1,11 +1,13 @@
 import PropTypes from "prop-types";
 import { useState, useEffect } from "react";
 import * as XLSX from "xlsx";
+import "./ExcelConverter.css";
 
 const ExcelConverter = ({ pdfFile }) => {
   const [data, setData] = useState([]);
   const [lessons, setLessons] = useState([]);
   const [conflicts, setConflicts] = useState([]);
+  const [warnings, setWarnings] = useState([]);
   const [fileUploaded, setFileUploaded] = useState(false);
   const [dataModified, setDataModified] = useState(false);
 
@@ -29,33 +31,137 @@ const ExcelConverter = ({ pdfFile }) => {
     setLessons(lessonList);
   };
 
+  // const checkForConflicts = () => {
+  //   let errors = [];
+  //   lessons.forEach((entry, index) => {
+  //     const { day, time, prof, cabinet, subject } = entry;
+  //     const key = `${day}-${time}-${cabinet}`;
+
+  //     if (!errors[key]) {
+  //       errors[key] = [{ index, prof, subject }];
+  //     } else {
+  //       const conflictObjects = errors[key];
+  //       const hasConflict = conflictObjects.some(obj => (
+  //         obj.index !== index &&
+  //         (obj.prof !== prof || obj.subject !== subject)
+  //       ));
+
+  //       if (hasConflict) {
+  //         console.error("Conflict found:", entry);
+  //       } else {
+  //         conflictObjects.push({ index, prof, subject });
+  //       }
+  //     }
+  //   });
+
+  //   setConflicts(errors);
+  //   setDataModified(false);
+  // };
   const checkForConflicts = () => {
-    let errors = [];
-    lessons.forEach((entry, index) => {
-      const { day, time, prof, cabinet, subject } = entry;
-      const key = `${day}-${time}-${cabinet}`;
+    let newConflicts = []; // Initialize a local array for conflicts
+    newConflicts = newConflicts.concat(checkForProfessorConflicts());
+    newConflicts = newConflicts.concat(checkForSubjectConflicts());
+    setConflicts(newConflicts); // Set the conflicts state with the combined conflicts
+    setDataModified(false);
 
-      if (!errors[key]) {
-        errors[key] = [{ index, prof, subject }];
+    let newWarnings = [];
+    newWarnings = newWarnings.concat(checkMaxLessonsPerDay());
+    setWarnings(newWarnings);
+  }
+
+  const checkForProfessorConflicts = () => {
+    const conflictsFound = [];
+    const seenLessons = {};
+  
+    lessons.forEach((lesson) => {
+      const { day, time, prof, cabinet } = lesson;
+      const lessonKey = `${day}-${time}-${prof}`;
+  
+      if (seenLessons[lessonKey]) {
+        seenLessons[lessonKey].forEach((existingLesson) => {
+          if (existingLesson.cabinet !== cabinet) {
+            conflictsFound.push({
+              conflictType: 'Professor',
+              lessonA: existingLesson,
+              lessonB: lesson,
+            });
+          }
+        });
+        seenLessons[lessonKey].push(lesson);
       } else {
-        const conflictObjects = errors[key];
-        const hasConflict = conflictObjects.some(obj => (
-          obj.index !== index &&
-          (obj.prof !== prof || obj.subject !== subject)
-        ));
-
-        if (hasConflict) {
-          console.error("Conflict found:", entry);
-        } else {
-          conflictObjects.push({ index, prof, subject });
-        }
+        seenLessons[lessonKey] = [lesson];
       }
     });
-
-    setConflicts(errors);
-    setDataModified(false);
+  
+    // conflictsArray.push(...conflictsFound);
+    return conflictsFound;
   };
 
+  const checkForSubjectConflicts = () => {
+    const subjectConflictsFound = [];
+    const seenSubjectLessons = {};
+  
+    lessons.forEach((lesson) => {
+      const { day, time, cabinet, subject } = lesson;
+      const lessonKey = `${day}-${time}-${cabinet}`;
+  
+      if (seenSubjectLessons[lessonKey]) {
+        seenSubjectLessons[lessonKey].forEach((existingLesson) => {
+          if (existingLesson.subject !== subject) {
+            subjectConflictsFound.push({
+              conflictType: 'Subject',
+              lessonA: existingLesson,
+              lessonB: lesson,
+            });
+          }
+        });
+        seenSubjectLessons[lessonKey].push(lesson);
+      } else {
+        seenSubjectLessons[lessonKey] = [lesson];
+      }
+    });
+      // conflictsArray.push(...combinedConflicts);
+    return subjectConflictsFound;
+  };
+
+  const checkMaxLessonsPerDay = () => {
+    const lessonsPerDayGroup = {};
+  
+    lessons.forEach((lesson) => {
+      const { day, group } = lesson;
+      const lessonKey = `${day}-${group}`;
+  
+      if (lessonsPerDayGroup[lessonKey]) {
+        lessonsPerDayGroup[lessonKey]++;
+      } else {
+        lessonsPerDayGroup[lessonKey] = 1;
+      }
+    });
+  
+    const exceedingLessons = Object.entries(lessonsPerDayGroup)
+      .filter(([, count]) => count > 5)
+      .reduce((acc, [key]) => {
+        const lesson = lessons.find((lesson) => {
+          const { day, group } = lesson;
+          return `${day}-${group}` === key;
+        });
+  
+        // const entry = { day: lesson.day, group: lesson.group };
+        if (!acc.some((item) => item.day === lesson.day && item.group === lesson.group)) {
+          const formattedString = `On ${lesson.day}, the group ${lesson.group} has more than 5 lessons!`;
+          acc.push(formattedString);
+        }
+        return acc;
+      }, []);
+  
+    return exceedingLessons;
+  };
+  
+  
+  
+  
+  
+  
 	const handleFileUpload = (e) => {
 		const reader = new FileReader();
 		reader.readAsBinaryString(e.target.files[0]);
@@ -98,6 +204,11 @@ const ExcelConverter = ({ pdfFile }) => {
 	useEffect(() => {
 		console.log(data);
 	}, [data]);
+
+  useEffect(() =>{
+    console.log(conflicts.length);
+    console.log(conflicts);
+  }, [conflicts])
 
   const handleCellEdit = (rowIndex, colIndex, newValue) => {
     const newData = data.map((row, rIndex) => {
@@ -153,16 +264,31 @@ const ExcelConverter = ({ pdfFile }) => {
 				</table>
 			)}
 
-      {conflicts.length > 0 && (
-        <div>
-          <h3>Conflicts:</h3>
-          <ul>
-            {conflicts.map((conflict, index) => (
-              <li key={index}>{JSON.stringify(conflict)}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+{conflicts.length > 0 && (
+  <div>
+    <h3>Conflicts:</h3>
+    <ul>
+      {conflicts.map((conflict, index) => (
+        <li key={index} style={{color:'#ff0033'}}>
+          {`${conflict.conflictType} conflict: ${conflict.lessonA.day} | ${conflict.lessonA.time} | ${conflict.lessonA.group} ${conflict.lessonA.subject} | ${conflict.lessonB.group} ${conflict.lessonB.subject}`}
+        </li>
+      ))}
+    </ul>
+  </div>
+)}
+
+{warnings.length > 0 && (
+  <div>
+    <h3>Warnings:</h3>
+    <ul>
+      {warnings.map((warning, index) => (
+        <li key={index} style={{color:'#eed202'}}>
+          {`Warning: ${warning}`}
+        </li>
+      ))}
+    </ul>
+    </div>
+)}
 
 			<br />
 			<br />
